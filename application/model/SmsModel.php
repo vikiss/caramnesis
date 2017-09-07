48a322a100e8e89821e61beaa82f7637fe28fcef
vikiss <vikis@skdn.com>
2017-09-07 13:40:36 +0300

    security update (sms)

--- application/model/SmsModel.php
+++ application/model/SmsModel.php
@@ -7,17 +7,17 @@ class SmsModel
         if ((!$to) or (!is_numeric($to)) or (strlen($message) < 5)) {
             return false;
         }
-        
-        $params =  array('username' => 'sknd',
-                         'password' => '18t9AHjqEPhu',
+
+        $params =  array('username' => Config::get('SMS_USER'),
+                         'password' => Config::get('SMS_PW'),
                          'phoneno' => $to,
                          'message' => $message,
                          );
-        
+
        $client = new SoapClient("http://smsgates.lt/beit/sms.asmx?WSDL");
        $sending =  $client->__soapCall("Send", array($params)); //stdClass Object ( [SendResult] => OK )
        if ($sending->SendResult == 'OK') return true;
-      
+
       /*usage:
        *$kas = SmsModel::SendSms(37069967248, 'pas mal');
        */
@@ -25,10 +25,10 @@ class SmsModel
 
         return false;
     }
-    
+
     public static function OkToSendSMS($country) // make sure it's not the middle of the night at the recipients place
     //2-letter country code, returns true if OK to send, false if not. OK to send - 9AM to 8PM
-    //usage: if (SmsModel::OkToSendSMS('US')) 
+    //usage: if (SmsModel::OkToSendSMS('US'))
     {
      $diff = self::getTimeDifference($country);
      $diff = explode(':', $diff); //sign(plus/minus):hours:minutes
@@ -40,7 +40,7 @@ class SmsModel
       else
       {return false;}
     }
-    
+
     public static function getTimeDifference($country) //read UTC offset field from countries table
     {
         $database = DatabaseFactory::getFactory()->getConnection();
@@ -51,7 +51,7 @@ class SmsModel
             return $result->offset;
         } else {return '+:00:00';}  //safe default return if country code not found
     }
-    
+
     public static function sendSMSNotification($recipient, $message) //checks if it's ok to send SMS and sends it
     //always returns true; enters sms in deferredSMS table if it's not OkToSensSMS at the time
     {
@@ -64,8 +64,8 @@ class SmsModel
      }
      return true;
     }
-    
-    
+
+
     public static function sendQueudSms() { //sends all queud SMS
         $database = DatabaseFactory::getFactory()->getConnection();
         $query = $database->prepare("SELECT * FROM deferredSMS;");
@@ -75,10 +75,10 @@ class SmsModel
          self::sendSMSNotification($row->recipient, $row->content);
          self::deleteQueudSms($row->id);
         }
-     
+
     }
- 
- 
+
+
     public static function getUserPhoneForSms($uuid) { //gets the phone number if the user has enabled SMS notification, false otherwise
         $database = DatabaseFactory::getFactory()->getConnection();
         $query = $database->prepare("SELECT user_phone, send_sms
@@ -89,12 +89,12 @@ class SmsModel
          $phone_no = $result->user_phone;
          $phone_no = filter_var($phone_no, FILTER_VALIDATE_INT);
          if ((strlen($phone_no) > 5) && ($result->send_sms == '1')) {
-            return $phone_no;          
+            return $phone_no;
          }
         }
         return false;
             }
-            
+
       public static function queueDeferredSMS($recipient, $message)
        {
         $database = DatabaseFactory::getFactory()->getConnection();
@@ -107,9 +107,9 @@ class SmsModel
       		if ($count == 1) {			return true;		}
       		return false;
        }
-       
-       
-       
+
+
+
            public static function deleteQueudSms($sms_id)
     {
         if (!$sms_id) {
@@ -128,5 +128,5 @@ class SmsModel
 
         return false;
     }
-            
+
 }
